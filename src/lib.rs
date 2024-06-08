5304973c506dbadd2d1b098300f0343e67b01f84
Shivaji Chalise <respond2shivaji@gmail.com>
2024-06-08 20:28:22 +0545

    adding print todos

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader, BufWriter, Read, Write},
+};
+
+pub struct Arguments {
+    pub action: String,
+    pub data: String,
+    pub file: File,
+}
+
+impl Arguments {
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Arguments, String> {
+        args.next(); // the first arg will be the path of the executable
+
+        let action = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get the action to be done".to_string()),
+        };
+
+        let mut data = String::new();
+
+        match action.as_str() {
+            "add" => {
+                data = match args.next() {
+                    Some(arg) => arg,
+                    None => return Err("Didn't get the todo that is to be added.".to_string()),
+                };
+            }
+            "delete" => {
+                data = match args.next() {
+                    Some(arg) => arg,
+                    None => return Err("Didn't get the todo id that is to be deleted.".to_string()),
+                };
+            }
+            "list" => {}
+            _ => {
+                return Err(format!(
+                    "Unknown action: {}. Only add, list and delete is supported.",
+                    action
+                ));
+            }
+        }
+
+        let file = File::options()
+            .read(true)
+            .create(true)
+            .append(true)
+            .open("todos.txt")
+            .unwrap();
+
+        Ok(Arguments { action, data, file })
+    }
+}
+
+pub fn list(arguments: Arguments) -> Result<(), Box<dyn Error>> {
+    let mut reader = BufReader::new(arguments.file);
+    let mut todos = String::new();
+
+    reader.read_to_string(&mut todos)?;
+
+    println!("{}", todos);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+use std::process;
+
+use garnu_parne::Arguments;
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = Arguments::build(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing args: {err}");
+        process::exit(1);
+    });
+
+    if arguments.action == "list" {
+        if let Err(e) = garnu_parne::list(arguments) {
+            eprintln!("Application error: {}", e);
+            process::exit(1);
+        }
+    }
 }
